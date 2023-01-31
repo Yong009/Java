df8a955d4ac18aa42950760abe0fbd82d5092f46
admin <admin@YD05-06>
2023-01-31 17:58:04 +0900

    1/31

---
+++ homework/src/homework/homework3.java
@@ -0,0 +1,102 @@
+package homework;
+
+import java.util.Scanner;
+
+public class homework3 {
+ public static void main(String[] args) {
+
+	 
+	 
+	//문제 1
+	 
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.println("x값 입력>");
+	 
+	  int x = sc.nextInt();
+	  
+	 System.out.println("y값 입력>"); 
+	  int y = sc.nextInt();
+	 
+	 if(x>0 && y>0){
+		 System.out.println("제1사분면");
+	 } else if(x<0 && y>0) {
+		 System.out.println("제2사분면");
+	 } else if(x<0 && y<0) {
+		 System.out.println("�제3사분면");
+	 } else if(x>0 && y<0) { 
+		 System.out.println("제4사분면");
+	 }
+		 
+		 
+		 
+ 
+	 
+	 
+	 
+	//문제 2
+	 
+	System.out.println("해당 연도를 적어주세요");
+	int years = sc.nextInt();
+	if(years%4 == 0 && years%100 != 0) { 
+	 System.out.println("윤년입니다");
+	} else if(years%400==0) { 
+			System.out.println("윤년입니다");
+		} else{ 
+			System.out.println("윤년이 아닙니다");
+		} 			
+	
+	 
+	
+	//문제 3
+	
+     System.out.println("가위바위보를 입력하세요. 1.가위 2.바위 3.보");
+     int game = sc.nextInt(); 
+     
+     switch(game){
+    	 case 1 :
+    		 System.out.println("바위를 내셔야 이깁니다.");
+    		 break;
+    	 case 2 :
+    		 System.out.println("보를 내셔야 이깁니다.");
+    		 break;
+    	 case 3 :
+    		 System.out.println("가위를 내셔야 이깁니다.");
+    		 break;
+     }
+
+	//문제 4
+	 System.out.println(" 몇 단? ");
+     int m=sc.nextInt(); 	 
+     System.out.println("몇까지 곱할까요?");
+	 int n=sc.nextInt();	
+
+		 for(int i=m; i<=m; i++) {
+			 for(int j=1; j<=n; j++) {
+				 System.out.println(i+"x"+j+"="+(i*j));
+			 }
+		 }
+		 
+//		 
+	//문제 5 
+	    System.out.println("몇단까지할까요?"); 
+	    
+	    int ne = sc.nextInt();
+	     
+		 for(int i=1; i<=ne; i++) {
+			 for(int j=1; j<=ne; j++) {
+				 if(i==9 ||j==9 || (i*j/10)==9 || (i*j%10)==9) {
+					 System.out.print(j+"x"+i+"="+(i*j)+"\t");
+				 }	 
+				  else if(i!=9 || j!=9 || (i*j/10)!=9 || (i*j%10)!=9) {
+					 System.out.print(j+"x"+i+"="+('9')+"\t");
+				  } if(j==n) {
+					  System.out.print(j+"x"+i+"="+(i*j)+"\n");
+				  }
+
+			 } 
+		
+		  
+		 } 
+ }
+}
---
+++ homework/src/homework/homework4.java
@@ -0,0 +1,67 @@
+package homework;
+
+import java.util.Scanner;
+
+public class homework4 {
+	public static void main(String[] args) {
+		int[] arr1 = { 10, 20, 30, 50, 3, 60, -3 };
+		Scanner sc = new Scanner(System.in);
+		// 문제1
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] == 60) {
+
+				System.out.println(i);
+			}
+		}
+
+		// 문제2
+
+		for (int i = 0; i < arr1.length; i++) {
+			if (i != 3) {
+				System.out.print(arr1[i] + "\t");
+			}
+		}
+
+		// 문제3
+		int index = 0;
+		System.out.println("\n" + "인덱스번호>");
+		int a = sc.nextInt();
+		for (int i = 0; i < arr1.length; i++) {
+
+			if (i == a) {
+				arr1[i] = 1000;
+				System.out.println(arr1[i]);
+			}
+		}
+
+		// 문제4
+		int max = 0;
+		int min = arr1[0];
+
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] > max) {
+				max = arr1[i];
+
+			}
+			if (arr1[i] < min) {
+				min = arr1[i];
+
+			}
+		}
+		System.out.println("최댓값  : " + max);
+		System.out.println("최솟값 : " + min);
+
+		// 문제5
+		int[] arr2 = new int[10];
+		for (int i = 0; i <= 9; i++) {
+			System.out.println("양의 정수를 입력하세요");
+			int plus = sc.nextInt();
+			arr2[i] = plus;}
+	     for (int j=0; j<=9; j++){
+	    	 if (arr2[j]%3 == 0) {
+				System.out.println(arr2[j]);
+			}
+		}
+
+	}
+}
